9f30f406b1c4c3581fc36593864a94047f3ca84e
Sylvestr Lozynskyy <pan.totoro@gmail.com>
2017-09-23 21:48:17 +0300

    :D

---
+++ ntjc/src/main/java/ntjc/NTJC.java
@@ -0,0 +1,13 @@
+package ntjc;
+
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+public class NTJC {
+
+	public static void main(String[] args) {
+		Globals globals = JsePlatform.standardGlobals();
+//		globals.load();
+	}
+
+}
