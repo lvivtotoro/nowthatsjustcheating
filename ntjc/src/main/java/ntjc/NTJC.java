c4ebff7492bfc85439a4b5a97b3b9317720053d7
Sylvestr Lozynskyy <pan.totoro@gmail.com>
2017-09-23 21:58:41 +0300

    First and only version has been completed

--- ntjc/src/main/java/ntjc/NTJC.java
+++ ntjc/src/main/java/ntjc/NTJC.java
@@ -1,13 +1,44 @@
 package ntjc;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.luaj.vm2.Globals;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import org.luaj.vm2.lib.jse.JsePlatform;
 
 public class NTJC {
 
+	public static String readProgram(String prog) {
+		InputStream in;
+		try {
+			in = new URL("https://raw.githubusercontent.com/lvivtotoro/nowthatsjustcheating/master/programs/" + prog
+					+ ".lua").openStream();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			byte[] buf = new byte[1024];
+			int len = 0;
+			while ((len = in.read(buf)) != -1) {
+				baos.write(buf, 0, len);
+			}
+			String body = new String(baos.toByteArray(), "UTF-8");
+			return body;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
+		if (args.length < 2)
+			throw new RuntimeException("Minimum arguments: 1");
 		Globals globals = JsePlatform.standardGlobals();
-//		globals.load();
+		globals.set("args", CoerceJavaToLua.coerce(args));
+		globals.load(readProgram(args[0])).call();
 	}
 
 }
