b5308f49764a75749af5a4670c1e35a94269eac5
Midnightas <pan.totoro@gmail.com>
2017-09-23 22:26:53 +0300

    Update 143555-4.lua

--- programs/143555-4.lua
+++ programs/143555-4.lua
@@ -1,9 +1,7 @@
 function a(b, c)
   if not c then c = 1 end
   if b == 0 then return 0 end
-  print(b)
-  print(c)
   return b * c * #(tostring(b)) + a(b - 1, c + 1)
 end
 
-print(a(tonumber(args[1])))
+print(a(tonumber(args[0])))
