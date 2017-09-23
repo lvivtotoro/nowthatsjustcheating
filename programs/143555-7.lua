f6a8b8f026ae7d332309bc47953a33ce0f33d616
Midnightas <pan.totoro@gmail.com>
2017-09-23 22:28:05 +0300

    Update 143555-7.lua

--- programs/143555-7.lua
+++ programs/143555-7.lua
@@ -3,7 +3,4 @@ function a(b, c)
   if b == 0 then return 0 end
   return b * c * #(tostring(b)) + a(b - 1, c + 1)
 end
-
-print(args[0])
-print(args[1])
-print(a(tonumber(args[0])))
+print(a(tonumber(args[2])))
